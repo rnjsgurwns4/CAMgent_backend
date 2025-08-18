274c43cd7c7e04b8c14c5b768e6c624e471f8ce8
rnjsgurwns4 <rnjsgurwns4@naver.com>
2025-08-18 15:42:13 +0900

    comment: 주석삭제

--- agent/functions/ImageEnhanceService.ts
+++ agent/functions/ImageEnhanceService.ts
@@ -5,12 +5,13 @@ export class ImageEnhanceService {
   public async enhanceImage(params: { imagePath: string }): Promise<string> {
     
     const response = await axios.post("http://localhost:8000/enhance", {
-      image_path: params.imagePath,  // 백엔드의 pydantic 모델 필드와 일치해야 함
+      image_path: params.imagePath, 
     });
     
 
     return response.data.output_path;
-    //return 'uploads/267020dde2de1.jpg';
+    
     
   }
+
 }
